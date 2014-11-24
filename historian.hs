0de13562d2b1a03d6432e89b5b13e253938cc82d
Marcin Kuptel <marcin@swiftkey.net>
2014-11-24 16:21:53 +0000

    Our dojo work

--- historian.hs
+++ historian.hs
@@ -1,10 +1,21 @@
 import Data.List
 import Data.List.Split
 import System.Process
+import qualified Data.Map as M
 
+ft :: Ord a => [a] -> M.Map a Int
+ft [] = M.empty
+ft (h:t) = M.insertWith (+) h 1 (ft t)
+
+extract :: String -> (String, [String])
+extract x = (author, words comment)
+	where [author, comment] = splitOn "\t" x 
 
 -- The program - a function from a list of strings (git log) to another list of strings (output)
 analyseHistory :: [String] -> [String]
+--analyseHistory x = ft (concat (map ( snd . extract ) x))
+
+
 ----- YOUR CODE HERE -----
 analyseHistory log = log                                            -- echo
 --analyseHistory log = [(show (length log)) ++ " commits"]            -- commit count
@@ -12,13 +23,13 @@ analyseHistory log = log                                            -- echo
 
 
 -- You could use/adapt this as sample input to 'analyseHistory'
-egInput = ["aaaaaaa\t10000\tAlice\tInitial commit",
-           "bbbbbbb\t20000\tBob\tBreak something",
-           "ccccccc\t30000\tAlice\tFix it!"]
+egInput = ["Alice\tInitial commit",
+           "Bob\tBreak something",
+           "Alice\tFix it!"]
 
 -- Customise this if you want different info from git (see "git log --help")
 --   hash <tab> timestamp <tab> author <tab> subject
-gitLogArgs = ["--pretty=%h%x09%at%x09%an%x09%s"]
+gitLogArgs = ["--pretty=%an%x09%s"]
 
 
 --------------------------------------------------------------------------------
