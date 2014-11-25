cba051e4d475a84c11d86ff63db32ea3cddb6fac
Emlyn Corrin <emlyn@swiftkey.net>
2014-11-25 11:18:44 +0000

    wordcloud!

--- historian.hs
+++ historian.hs
@@ -1,68 +1,74 @@
 import Data.List
+import Data.Tuple
 import Data.Set
+import Data.Ord
 import Data.List.Split
-import System.Process
 import Data.MultiSet
+import Text.Regex.Posix
+import System.Process
+import Graphics.GD
+import Graphics.WordCloud
 
 freq :: Ord a => [a] -> [(a, Occur)]
 freq = toOccurList . Data.MultiSet.fromList
 
 listWords :: [String] -> [String]
-listWords = concat . Data.List.map (words . last . splitOn "\t")
+listWords = concat . Data.List.map words
 
 bannedWords = Data.Set.fromList ["'master'", "master", "in", "of", "for", "the", "Merge", "and", "when", "with", "is", "not",
- "a", "an", "branch", "to", "into", "on", "from", "-"]
+ "a", "an", "branch", "to", "into", "on", "from", "-", "pull", "request", "Minor:"]
+goodWord :: String -> Bool
+goodWord w = not ((Data.Set.member w bannedWords) ||
+                  (w =~ "(git@)?github.com:.*" :: Bool) ||
+                  (w =~ "(richardtunnicliffe|tihancock)/.*" :: Bool))
 
-freqWords :: (Set String) -> [String] -> [(Occur, String)]
-freqWords banned log = (reverse (sort [(s, f) | (f, s) <- (freq [x | x <- (listWords log), (not (Data.Set.member x banned))])]))
+freqWords :: (String -> Bool) -> [String] -> [(String, Occur)]
+freqWords pred log = (reverse (sortBy (comparing swap)
+                                        (freq (Data.List.filter pred (listWords log)))))
 
 -- The program - a function from a list of strings (git log) to another list of strings (output)
 analyseHistory :: [String] -> [String]
------ YOUR CODE HERE -----
---analyseHistory log = log                                            -- echo
---analyseHistory log = [(show (length log)) ++ " commits"]            -- commit count
---analyseHistory = sort . concat . map (words . last . splitOn "\t")  -- sorted list of words in commit subjects
+analyseHistory log = Data.List.map show (take 20 (freqWords goodWord log))
 
--- This will output a list of most common words ordered by use on the commit messages of the repo
-analyseHistory log = Data.List.map show (take 20 (freqWords bannedWords log))
--- e.g.
--- > run "."
--- (2,"some")
--- (2,"Add")
--- (1,"tabs")
--- (1,"setup")
--- (1,"links")
--- (1,"instructions")
--- (1,"install")
--- (1,"input,")
--- (1,"example")
--- (1,"dojo")
--- (1,"de-ponce")
--- (1,"commit")
--- (1,"code")
--- (1,"Names,")
--- (1,"Minorly")
--- (1,"Initial")
--- (1,"'split'")
+conf :: Config
+conf = Config {
+            confFontFamily = FontName "Arial"
+          , confFontSize   = 60
+          , confMaxWords   = 80
+          , confFontSizeMin = 8
+          , confCanvasSize = (1024,768)
+          , confDefaultPos = (512,376)
+          , confColor      = (100,230,180)
+          , confBGColor    = (0,0,0)
+          , confCloudAlgo  = Circular }
+
+wordcloud :: [String] -> IO Image
+wordcloud log = (makeCloud conf (take 100 (freqWords goodWord log)))
 
 -- You could use/adapt this as sample input to 'analyseHistory'
-egInput = ["aaaaaaa\t10000\tAlice\tInitial commit",
-           "bbbbbbb\t20000\tBob\tBreak something",
-           "ccccccc\t30000\tAlice\tFix it!"]
+egInput = ["Initial commit",
+           "Break something",
+           "Fix it!"]
 
 -- Customise this if you want different info from git (see "git log --help")
 --   hash <tab> timestamp <tab> author <tab> subject
-gitLogArgs = ["--pretty=%h%x09%at%x09%an%x09%s"]
+gitLogArgs = ["--pretty=%s"]
 
 --------------------------------------------------------------------------------
 
+saveimg :: String -> IO Image -> IO ()
+saveimg fname ioimg = do img <- ioimg
+                         savePngFile fname img
+
 -- Runs everything (including 'git log') straight from Haskell (maybe useful for interactive testing).
 -- Usage: ghci> run "path/to/repo"
-run :: String -> IO ()
-run repoDir = gitLog >>= (putStr . unlines . analyseHistory . lines)
+run :: String -> String -> IO ()
+run fname repoDir = gitLog >>= (saveimg fname . wordcloud . lines)
   where gitLog = readProcess "git" (["-C", repoDir, "log"] ++ gitLogArgs) ""
 
 -- Pipe in git logs, pipe out results
 -- Usage: $ git log --pretty=... | runhaskell /path/to/historian.hs
 main :: IO ()
-main = interact (unlines . analyseHistory . lines)
+main = do
+    contents <- getContents
+    saveimg "wordcloud.png" (wordcloud (splitOn "\n" contents))
